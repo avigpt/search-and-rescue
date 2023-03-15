fix value-iter off-by-one, clean out data files, change data density label to 'd', add policies folder, make found reward a constant, automate num_states in q.py

q.py
@@ -6,7 +6,6 @@ import pandas as pd
 import sys
 import time
 
-NUM_STATES = 9
 NUM_ACTIONS = 4
 DISC = 0.95
 NUM_EPOCHS = 100
@@ -14,9 +13,8 @@ LEARN_RATE = 0.1
 DIFF = 0.001
 
 actions_from_state = {} # state -> possible actions in data
-q_table = np.random.rand(NUM_STATES, NUM_ACTIONS)
 
-def update(row):
+def update(row, q_table):
     s = row['s']
     a = row['a']
     r = row['r']
@@ -40,20 +38,20 @@ def populate_actions(data):
             actions_from_state[s] = set()
         actions_from_state[s].add(a)
 
-def learn(data):
+def learn(data, q_table, num_states):
     epoch = 0
     while epoch < NUM_EPOCHS:
         converge_count = 0
         for _, row in data.iterrows():
-            converge_count += update(row)
-            if converge_count == NUM_STATES:
+            converge_count += update(row, q_table)
+            if converge_count == num_states:
                 return
         epoch += 1
 
-def write_policy(filename):
+def write_policy(filename, q_table):
     plot_actions = []
     max_actions_i = np.argmax(q_table, axis=1)
-    with open(filename, 'w') as f:
+    with open(f"policies/{filename}", 'w') as f:
         for action in max_actions_i:
             plot_actions.append(action)
             f.write(f"{action + 1}\n")
@@ -88,8 +86,10 @@ def main():
     infile = sys.argv[1]
     data = pd.read_csv("data/" + infile)
     populate_actions(data)
-    learn(data)
-    policy = write_policy(sys.argv[2])
+    num_states = len(actions_from_state)
+    q_table = np.random.rand(num_states, NUM_ACTIONS)
+    learn(data, q_table, num_states)
+    policy = write_policy(sys.argv[2], q_table)
     end = time.time()
     print("Time: ", end - start)
     policy_as_grid = get_policy_grid(policy)